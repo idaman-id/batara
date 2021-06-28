25db7ae2076586d88fb615690cc255f4062be30b
Raka Suryaardi Widjaja <raka.suryadi@gmail.com>
2021-06-28 14:23:22 +0700

    [FIX] App doesnt required Provider

--- src/Application.ts
+++ src/Application.ts
@@ -4,7 +4,6 @@ global.__basedir = __dirname;
 import express from 'express';
 import IApplication from './contracts/interface/Application';
 import ILanguage from './contracts/interface/ValidationLanguage';
-import Provider from './providers/Provider';
 
 declare global 
 {
@@ -25,25 +24,15 @@ export default class Application
 
   instance: express.Application;
   setting: IApplication;
-  providers: Array<Provider>;
 
-  constructor(setting: IApplication, providers: Array<Provider>)
+  constructor(setting: IApplication)
   {
     this.instance = express();
     this.setting = setting;
-    this.providers = providers;
-  }
-
-  setup(): void
-  {
-    this.providers.forEach(provider => {
-      provider.register();
-    });
   }
 
   start(): void
   {
-    this.setup();
     this.instance.listen(this.setting.port, this.setting.host, () => {
       console.info(`[${this.setting.environment.toUpperCase()}] ${this.setting.name} is running on ${this.setting.host}:${this.setting.port}`);
     });
